cd7e5b1118eb95d9c5dffd2e5ad9b0dd3dffca9c
Swaroop-007 <58040710+Swaroop-007@users.noreply.github.com>
2021-08-16 16:55:47 +0530

    Add files via upload

---
+++ PublicationTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myappAug10
+{   class Publications
+    {
+        
+        public Publications(string title, float price)
+        {
+            this.title = title;
+            this.price = price;
+        }
+        public float price { get; set; }
+
+        public string title { get; set; }
+        public virtual string Display()
+        {
+            return "";
+        }
+        
+
+    }
+
+    class Book : Publications
+    {
+        
+        public Book(string title, float price, int pagecnt): base(title,price)
+        {
+            this.pagecnt = pagecnt;
+        }
+
+        public int pagecnt { get; set; }
+
+        
+        public override string Display()
+        {
+            return $"The title of the Book is {this.title} for price {this.price} and page count is {this.pagecnt}";
+        }
+    }
+
+    class Video: Publications
+    {
+        float playtime;
+
+        public Video(string title, float price, float playtime) : base(title, price)
+        {
+            this.playtime = playtime;
+        }
+        
+        public float palytime { get; set; }
+       
+        public override string Display()
+        {
+           return $"The title of the video is {this.title} for price {this.price} and playtime is {this.playtime}";
+        }
+
+
+    }
+    class PublicationTest
+    {
+        public static void Main()
+        {
+            Book b1 = new Book("Harry Potter",255,252);
+            Video v1 = new Video("Harry Potter", 600, 132);
+            Console.WriteLine(b1.Display());
+            Console.WriteLine(v1.Display());
+            
+            
+        }
+    }
+}
